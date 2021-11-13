df89d335270b1d72ea7f49852d780ac80e0543d9
Suhyeon Lee <esudatcom@gmail.com>
2021-11-14 01:39:57 +0900

    feat.11 switch 문

---
+++ part1_012.js
@@ -0,0 +1,16 @@
+const subject = "javascript";
+
+switch (subject) {
+  case "C++":
+    console.log("초보자를 위한 C++200제");
+    break;
+  case "javascript":
+    console.log("초보자를 위한 자바스크립트 200제");
+    break;
+  case "python":
+    console.log("초보자를 위한 파이썬 200제");
+    break;
+  default:
+    console.log("이제 초보자가 아닙니다.");
+    break;
+}
