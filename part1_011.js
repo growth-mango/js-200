1d94a1ea6ad882c4c60cae62ece72a4570e802a1
Suhyeon Lee <esudatcom@gmail.com>
2021-11-13 17:20:56 +0900

    feat: 11번 문제 풀이 완료

--- part1_011.js
+++ part1_011.js
@@ -1,2 +1,12 @@
 //조건문 if, else, if else
 const number = 2;
+
+if (number == 1) {
+  console.log("number 는 1 입니다.");
+} else if (number == 2) {
+  console.log("number 는 2 입니다.");
+} else if (number == 3) {
+  console.log("number 는 3 입니다.");
+} else {
+  console.log("number 는 1,2,3 외 다른 숫자입니다.");
+}
