6780270e7565d79979fd78abc4220d37e9aefdeb
Suhyeon Lee <esudatcom@gmail.com>
2021-11-16 23:46:58 +0900

    feat.js200 파트 22번

---
+++ part2_024.js
@@ -0,0 +1,8 @@
+const condition = 5 > 10;
+condition ? console.log("left") : console.log("right");
+
+let result = condition
+  ? (console.log("삼항 연산식의 첫 번째 표현식 입니다."), "표현식1")
+  : (console.log("삼항 연산식의 두 번째 표현식 입니다."), "표현식2");
+
+console.log(result);
