a8f18b959cfc1e57e9ba151c957ce1d7234d6c8c
Suhyeon Lee <esudatcom@gmail.com>
2021-11-16 23:47:24 +0900

    js200 파트 22

---
+++ part2_022.js
@@ -0,0 +1,23 @@
+/*
+// 비교 대상값의 자료형이 서로 다르면 강제로 형을 바꾼 뒤에 비교
+console.log(5 == 5);
+console.log("5" == 5);
+console.log("5" === 5);
+*/
+
+// and 논리 연산자 &&
+console.log(true && true);
+console.log(true && false);
+console.log("문장" == "문장" && 5 == "5");
+
+// or 논리 연산자 ||
+console.log(true || false);
+console.log(true || true);
+console.log(false || false);
+
+// not 논리 연산자 !
+console.log(!true);
+console.log(!false);
+console.log(!5);
+console.log(!"문장");
+console.log(!!5);
