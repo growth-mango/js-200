3959a304231b504e1d52707eb9effc19ef5b62ea
Suhyeon Lee <esudatcom@gmail.com>
2021-11-28 23:55:54 +0900

    feat:jeju100 7번 변수명

---
+++ jeju100/jeju100_007.js
@@ -0,0 +1,15 @@
+const age = 10;
+console.log(age);
+
+//const &age = 20;
+//console.log(&age)
+
+//const let = 30
+//console.log(let)
+
+const _age = 40;
+console.log(_age);
+
+
+const 1age = 50;
+console.log(1age);
