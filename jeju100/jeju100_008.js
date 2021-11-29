5955b2b82f5bc451a0411c6180f1894dde9871b0
Suhyeon Lee <esudatcom@gmail.com>
2021-11-30 00:05:02 +0900

    feat:jeju100 9번 concat 활용하여 데이터 출력

---
+++ jeju100/jeju100_008.js
@@ -0,0 +1,12 @@
+const d = {
+  height: 100,
+  weight: 78,
+  weight: 84,
+  temperature: 36,
+  eyesight: 1,
+};
+
+console.log(d["weight"]);
+//키가 중복되었을 경우 마지막 키의 값을 가지고 옴
+console.log(d.eyesight);
+//대괄호 접근자는 따옴표 해야함. 점 접근자는 따옴표 생략 가능데쓰
