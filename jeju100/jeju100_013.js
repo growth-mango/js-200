d366ce71d78c72a9d89a252cc025640df1426bfa
Suhyeon Lee <esudatcom@gmail.com>
2021-12-01 00:42:18 +0900

    feat: jeju100 오브젝트와 prompt

---
+++ jeju100/jeju100_013.js
@@ -0,0 +1,13 @@
+const planets = {
+  1: "수성",
+  2: "금성",
+  3: "지구",
+  4: "화성",
+  5: "목성",
+  6: "토성",
+  7: "천왕성",
+  8: "해왕성",
+};
+
+const input = prompt("몇 번째 행성을 원하나요?");
+console.log(planets[input]);
