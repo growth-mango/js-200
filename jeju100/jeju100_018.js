c3e76cb252ddcf94a036217e9fc5ac25b47a8e2f
Suhyeon Lee <esudatcom@gmail.com>
2021-12-05 15:04:25 +0900

    18번 수정

--- jeju100/jeju100_018.js
+++ jeju100/jeju100_018.js
@@ -1,5 +1,16 @@
+/*
 function avg(korean, math, english) {
   return (korean + math + english) / 3;
 }
 
 console.log(Math.floor(avg(89, 91, 95)));
+*/
+
+const score = prompt("국어 수학 영어 점수를 입력하세요").split(" ");
+let sum = 0;
+
+for (let i = 0; i < score.length; i++) {
+  sum = sum + parseInt(score[i], 10);
+}
+
+console.log(Math.floor(sum / score.length));
