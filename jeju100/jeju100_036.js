193699b187048c0fd30a721fffdea93abc4585a9
Suhyeon Lee <esudatcom@gmail.com>
2021-12-12 18:04:32 +0900

    feat:jeju100 36번문제 구구단

---
+++ jeju100/jeju100_036.js
@@ -0,0 +1,19 @@
+/*
+const dan = 11;
+let result = "";
+
+for (let i = 1; i <= 9; i++) {
+  result = result + dan * i + " ";
+}
+
+console.log(result);
+*/
+
+const dan = 2;
+let result = "";
+
+for (let i = 1; i <= 9; i++) {
+  result = result + dan * i + " ";
+}
+
+console.log(result);
