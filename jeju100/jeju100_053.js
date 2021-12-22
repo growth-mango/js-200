9111a10a5db0cf1726ca49442cc2e4dd086ab8f4
Suhyeon Lee <esudatcom@gmail.com>
2021-12-23 00:21:42 +0900

    feat:jeju100 53번문제 괄호 형태 판단하기

---
+++ jeju100/jeju100_053.js
@@ -0,0 +1,42 @@
+//입력으로 주어진 괄호 문자열이 바른 문자열인지 판별
+
+function mathBrackets(arr) {
+  let count = 0;
+  //일단 괄호의 개수가 맞지 않으면(홀수면) false 임
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "(") {
+      count++;
+    }
+    if (arr[i] === ")") {
+      count--;
+    }
+  }
+  //count 가 0 인 경우에만 코드 작성. 아니면 false 를 return 하면 됨.
+  if (count !== 0) {
+    return false;
+  }
+
+  let bracket = [];
+  for (let i in arr) {
+    if (arr[i] === "(") {
+      bracket.push("(");
+    }
+    if (arr[i] === ")") {
+      if (bracket.length === 0) {
+        return false;
+      }
+      bracket.pop();
+    }
+    return true;
+  }
+}
+
+const n = "(((())".split("");
+console.log(n);
+console.log(mathBrackets(n));
+
+if (mathBrackets(n) === true) {
+  console.log("YES");
+} else {
+  console.log("NO");
+}
