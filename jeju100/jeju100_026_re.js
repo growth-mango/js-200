57bf6863184e933523121e4dceffb8a78d22e0cf
Suhyeon Lee <esudatcom@gmail.com>
2022-01-13 23:33:20 +0900

    feat:jeju100 26번 행성문제 오브젝트

---
+++ jeju100/jeju100_026_re.js
@@ -0,0 +1,13 @@
+const planet = {
+  수성: "Mercury",
+  금성: "Venus",
+  지구: "Earth",
+  화성: "Mars",
+  목성: "Jupiter",
+  토성: "Saturn",
+  천왕성: "Urnaus",
+  해왕성: "Neptune",
+};
+
+console.log(planet.수성);
+console.log(planet[목성]);
